8eb3b1ff854209c5634e8b6da776de26e86ac350
renqiang_hu <1306816138@qq.com>
2020-04-19 18:29:05 +0800

    新增

---
+++ src/vuex/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+   num: 1
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+
+  }
+})
+
+export default store
