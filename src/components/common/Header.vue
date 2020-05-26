7aa68cda5b5d8daba2810dcbf0bc37d4006a6988
caikuner <caikun0229@foxmail.com>
2020-05-26 16:40:46 +0800

    fix: 保养提醒的查询api待完善

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -196,7 +196,7 @@ export default {
             var date = new Date();
             var year = date.getFullYear();
             var month = date.getMonth() + 1;
-            var day = date.getDate() - 1;
+            var day = date.getDate();
             if (month < 10) {
                 month = '0' + month;
             }
